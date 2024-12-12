d51f12f22e3bbca7af9a903044a5d86d25edd238
Martine Karlsen <martine.karlsen02@gmail.com>
2024-12-12 18:25:20 +0100

    solution day 1

---
+++ Day 1/distances.py
@@ -0,0 +1,29 @@
+#day 1, part 1 & 2
+def calcDistAndSim(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+
+        leftNumbers = []
+        rightNumbers = []
+        distances = []
+        similarities = []
+        
+        for line in f:
+            left, right = line.split()
+            leftNumbers.append(int(left))
+            rightNumbers.append(int(right))
+        
+        for number in leftNumbers:
+            similarities.append(rightNumbers.count(number)*number)
+
+        leftNumbers.sort()
+        rightNumbers.sort()
+
+        for i in range(len(leftNumbers)):
+            distances.append(abs(leftNumbers[i]-rightNumbers[i]))
+
+    return sum(distances), sum(similarities)
+
+print(calcDistAndSim("wishlist.txt"))
+
+
+
