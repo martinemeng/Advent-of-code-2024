fc841e2a8d79b9f48221e4a8b52da44e237be6c4
Martine Karlsen <martine.karlsen02@gmail.com>
2024-12-14 21:06:50 +0100

    solution to part 2, day 2

--- Day 2/safe.py
+++ Day 2/safe.py
@@ -1,15 +1,31 @@
 ALLOWED = [1,2,3,-1,-2,-3]
 
+# helping function
+def is_safe(differences):
+    return all(diff in ALLOWED for diff in differences) and (all(diff > 0 for diff in differences) or all(diff < 0 for diff in differences))
+
 def checkSafe(filename):
     safe_reports = 0
     with open(filename, 'r', encoding='utf-8') as f:
         lines = f.readlines()
         for line in lines:
             numbers = list(map(int,line.strip().split()))
-            differences = [numbers[i]-numbers[i+1] for i in range(len(numbers)-1)]
-            if all(diff in ALLOWED for diff in differences):
-                if all(diff > 0 for diff in differences) or all(diff < 0 for diff in differences):
-                    safe_reports +=1
+            differences = []
+            for i in range(len(numbers)-1):
+                differences.append(numbers[i]-numbers[i+1])
+
+            if is_safe(differences):
+                safe_reports +=1
+            else:
+                for i in range(len(numbers)):
+                    # create new list to remove the single level, if safe without safe_reports+=1
+                    new_numbers = numbers[:i] + numbers[i+1:]
+                    new_differences = []
+                    for j in range(len(new_numbers) - 1):
+                        new_differences.append(new_numbers[j] - new_numbers[j + 1])
+                    if is_safe(new_differences):
+                        safe_reports += 1
+                        break 
     return safe_reports
 
 
